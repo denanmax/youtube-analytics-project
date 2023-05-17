76b1111b7afab2a27378e953c5536ec8fef376ef
denanmax <deshs93@gmail.com>
2023-05-17 20:32:28 +0200

    corrected according to FLASK8

--- src/channel.py
+++ src/channel.py
@@ -9,11 +9,15 @@ class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
         info = json.dumps(channel, indent=2, ensure_ascii=False)
         print(info)
